ae54d8e15f8103224d55a7e7439792df37315619
shadeemerhi <shadmerhi@gmail.com>
2022-04-04 16:05:55 -0600

    remove layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../chakra/theme";
-import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
