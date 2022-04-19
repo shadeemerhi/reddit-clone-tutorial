8a86307ebb3dc0be7fe7b1866deeaf707c9b4d25
shadeemerhi <shadmerhi@gmail.com>
2022-04-18 23:49:00 -0600

    link to submit page and page definition

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -7,6 +7,7 @@ import safeJsonStringify from "safe-json-stringify";
 import NotFound from "../../../components/Community/NotFound";
 import Header from "../../../components/Community/Header";
 import PageContent from "../../../components/Layout/PageContent";
+import CreatePostLink from "../../../components/Community/CreatePostLink";
 
 type CommunityPageProps = {
   communityData: Community;
@@ -24,7 +25,7 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
       <Header communityData={communityData} />
       <PageContent>
         <>
-          <div>LHS</div>
+          <CreatePostLink />
         </>
         <>
           <div>RHS</div>
---
+++ src/pages/r/[communityId]/submit.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const SubmitPostPage: React.FC = () => {
+  return <div>Submit page</div>;
+};
+export default SubmitPostPage;
